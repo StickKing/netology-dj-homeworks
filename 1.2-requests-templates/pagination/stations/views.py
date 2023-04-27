7b0309af85dfc24eea0cb782069699f9894ba6c5
Anton <mainsample@yandex.ru>
2023-04-27 13:22:42 +0300

    Решено второе задание + доп. задание

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.conf import settings
+from django.core.paginator import Paginator
+from pandas import read_csv
+from csv import DictReader
 
+BUS_STATIONS = list(DictReader(open(settings.BUS_STATION_CSV, newline='')))
 
 def index(request):
     return redirect(reverse('bus_stations'))
@@ -9,9 +14,11 @@ def index(request):
 def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
-
+    page_number = int(request.GET.get('page', 1))
+    paginator = Paginator(BUS_STATIONS, 10)
+    page = paginator.get_page(page_number)
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+         'bus_stations': page,
+         'page': page,
     }
     return render(request, 'stations/index.html', context)
--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -19,6 +19,42 @@ DATA = {
     # можете добавить свои рецепты ;)
 }
 
+def omlet_view(request):
+    servings = request.GET.get('servings')
+    if servings:
+        context = {
+            'recipe': { i: DATA['omlet'][i] * int(servings) for i in DATA['omlet'] }
+        }
+    else:
+        context = {
+            'recipe': DATA['omlet']
+        }
+    return render(request, 'calculator/index.html', context)
+
+def pasta_view(request):
+    servings = request.GET.get('servings')
+    if servings:
+        context = {
+            'recipe': { i: DATA['pasta'][i] * int(servings) for i in DATA['pasta'] }
+        }
+    else:
+        context = {
+            'recipe': DATA['pasta']
+        }
+    return render(request, 'calculator/index.html', context)
+
+def buter_view(request):
+    servings = request.GET.get('servings')
+    if servings:
+        context = {
+            'recipe': { i: DATA['buter'][i] * int(servings) for i in DATA['buter'] }
+        }
+    else:
+        context = {
+            'recipe': DATA['buter']
+        }
+    return render(request, 'calculator/index.html', context)
+
 # Напишите ваш обработчик. Используйте DATA как источник данных
 # Результат - render(request, 'calculator/index.html', context)
 # В качестве контекста должен быть передан словарь с рецептом:
